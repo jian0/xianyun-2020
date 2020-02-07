259fa6bf9d2949d22b3522f2251691c3ff31676e
jianren <365088870@qq.com>
2020-02-07 18:57:23 +0800

    02-创建静态页面

---
+++ pages/air/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="air">国内机票</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/post/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="post">旅游攻略</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
