904476f32f595ba238cc88364c8632d53d9f8065
jianren <365088870@qq.com>
2020-02-07 19:22:21 +0800

    03-默认样式，过渡动画，修改配置文件，头部和尾部组件[D的引入

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="container">
+    <Header></Header>
     <div>主页</div>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-
+import Header from "@/components/header.vue";
+import Footer from "@/components/header.vue";
 export default {
   components: {
+    Header,
+    Footer
   }
-}
+};
 </script>
 
 <style>
