8f7940a76ae12c0ccb342384b14ba348257c8cf4
jianren <365088870@qq.com>
2020-02-07 20:45:37 +0800

    05-固定图片资源实现轮播图

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,41 @@
 <template>
-  <div class="container">
-    <div>主页</div>
+  <div class="index">
+    <span class="demonstration"></span>
+    <el-carousel height="500px">
+      <el-carousel-item v-for="(item,index) in postList" :key="index">
+        <p class="img" 
+        :style="`background:url(${item.url}) center center no-repeat
+        background-size:contain contain`"></p>
+      </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
 
 <script>
 export default {
-  components: {
+  data () {
+    return {
+      postList:[
+        {
+          url:'http://157.122.54.189:9095/assets/images/th01.jfif'
+        },
+        {
+          url:'http://157.122.54.189:9095/assets/images/th02.jfif'
+        },
+        {
+          url:'http://157.122.54.189:9095/assets/images/th03.jfif'
+        },
+        {
+          url:'http://157.122.54.189:9095/assets/images/th04.jfif'
+        }
+      ]
+    }
   }
 };
 </script>
 
-<style>
+<style lang='less' scoped>
+.img {
+  height: 500px;
+}
 </style>
