5f0a182252f9f6bb8a49bf64796d5e4cf53dc92d
jianren <365088870@qq.com>
2020-02-12 21:30:36 +0800

    24-机票列表动态渲染数据

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -1,63 +1,73 @@
 <template>
-    <section class="contianer">
-        <el-row  type="flex" justify="space-between">
+  <section class="contianer">
+    <el-row type="flex" justify="space-between">
+      <!-- 顶部过滤列表 -->
+      <div class="flights-content">
+        <!-- 过滤条件 -->
+        <div></div>
 
-            <!-- 顶部过滤列表 -->
-            <div class="flights-content">
-                <!-- 过滤条件 -->
-                <div>
-                    
-                </div>
-                
-                <!-- 航班头部布局 -->
-                <flightsListHead>
-                    
-                </flightsListHead>
-                
-                
-                <!-- 航班信息 -->
-                <div>
-                    
-                </div>
-            </div>
+        <!-- 航班头部布局 -->
+        <flightsListHead></flightsListHead>
 
-            <!-- 侧边栏 -->
-            <div class="aside">
-                <!-- 侧边栏组件 -->
-            </div>
-        </el-row>
-    </section>
+        <!-- 航班信息 -->
+        <flightsItem v-for="(item,index) in dataList" :key="index" :data="item"></flightsItem>
+      </div>
+
+      <!-- 侧边栏 -->
+      <div class="aside">
+        <!-- 侧边栏组件 -->
+      </div>
+    </el-row>
+  </section>
 </template>
 
 <script>
-
 import moment from "moment";
 import flightsListHead from "@/components/air/flightsListHead";
+import flightsItem from "@/components/air/flightsItem";
 
 export default {
-    data(){
-        return {
-            
-        }
-    },
-    components:{
-        flightsListHead
+  data() {
+    return {
+      flightsData: {}, // 航班总数据
+      dataList: [] // 航班列表数据，用于循环flightsItem组件，单独出来是因为要分页
+    };
+  },
+  components: {
+    flightsListHead,
+    flightsItem
+  },
+  mounted() {
+    this.getData();
+  },
+  methods: {
+    // 获取航班总数据
+    getData() {
+      this.$axios({
+        url: "/airs",
+        params: this.$route.query
+      }).then(res => {
+        console.log(res);
+        this.flightsData = res.data
+        this.dataList = this.flightsData.flights
+      });
     }
-}
+  }
+};
 </script>
 
 <style scoped lang="less">
-    .contianer{
-        width:1000px;
-        margin:20px auto;
-    }
+.contianer {
+  width: 1000px;
+  margin: 20px auto;
+}
 
-    .flights-content{
-        width:745px;
-        font-size:14px;
-    }
+.flights-content {
+  width: 745px;
+  font-size: 14px;
+}
 
-    .aside{
-        width:240px;
-    } 
+.aside {
+  width: 240px;
+}
 </style>
