eb4ee6311838eb94280acf41969f06e924bc3870
jianren <365088870@qq.com>
2020-02-13 21:15:09 +0800

    27-分页功能完成

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -7,10 +7,24 @@
         <div></div>
 
         <!-- 航班头部布局 -->
-        <flightsListHead></flightsListHead>
+        <div>
+          <flightsListHead></flightsListHead>
 
-        <!-- 航班信息 -->
-        <flightsItem v-for="(item,index) in dataList" :key="index" :data="item"></flightsItem>
+          <!-- 航班信息 -->
+          <flightsItem v-for="(item,index) in dataList" :key="index" :data="item"></flightsItem>
+          <el-row type="flex" justify="center">
+            <!-- 分页功能 -->
+            <el-pagination
+              @size-change="handleSizeChange"
+              @current-change="handleCurrentChange"
+              :current-page="currentPage"
+              :page-sizes="[5, 10, 15, 20]"
+              :page-size="pageSize"
+              layout="total, sizes, prev, pager, next, jumper"
+              :total="flightsData.total"
+            ></el-pagination>
+          </el-row>
+        </div>
       </div>
 
       <!-- 侧边栏 -->
@@ -30,7 +44,9 @@ export default {
   data() {
     return {
       flightsData: {}, // 航班总数据
-      dataList: [] // 航班列表数据，用于循环flightsItem组件，单独出来是因为要分页
+      dataList: [], // 航班列表数据，用于循环flightsItem组件，单独出来是因为要分页
+      currentPage: 1, //当前页码
+      pageSize: 5, // 每页显示的条数
     };
   },
   components: {
@@ -48,9 +64,28 @@ export default {
         params: this.$route.query
       }).then(res => {
         console.log(res);
-        this.flightsData = res.data
-        this.dataList = this.flightsData.flights
+        this.flightsData = res.data;
+        // this.dataList = this.flightsData.flights;
+        this.setDataList()
       });
+    },
+    // 设置dataList数据
+    setDataList() {
+      const start = (this.currentPage - 1) * this.pageSize;
+      const end = start + this.pageSize;
+      this.dataList = this.flightsData.flights.slice(start, end);
+    },
+    // 分页
+    handleSizeChange(val) {
+      // console.log(`每页 ${val} 条`);
+      this.pageSize = val
+      this.currentPage = 1
+      this.setDataList()
+    },
+    handleCurrentChange(val) {
+      // console.log(`当前页: ${val}`);
+      this.currentPage = val
+      this.setDataList()
     }
   }
 };
--- plugins/element-ui.js
+++ plugins/element-ui.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Element from 'element-ui'
-import locale from 'element-ui/lib/locale/lang/en'
+// locale ： 语言环境  zh-CN ： 中文
+import locale from 'element-ui/lib/locale/lang/zh-CN'
 
 Vue.use(Element, { locale })
