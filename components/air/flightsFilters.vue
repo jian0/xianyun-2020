14f9b8a03590487e02c5c96a076a1477e25ee7db
jianren <365088870@qq.com>
2020-02-14 17:06:22 +0800

    33-优化筛选规则，把代码添加到computed里面进行监听

--- components/air/flightsFilters.vue
+++ components/air/flightsFilters.vue
@@ -52,6 +52,7 @@
       筛选：
       <el-button type="primary" round plain size="mini" @click="handleFiltersCancel">撤销</el-button>
     </div>
+    <span>{{filter}}</span>
   </div>
 </template>
 
@@ -76,44 +77,74 @@ export default {
       ]
     };
   },
+  computed: {
+    filter() {
+      const result = this.data.flights.filter(v => {
+        let vaild = true;
+        // 航空公司
+        if (this.company && v.airline_name !== this.company) {
+          vaild = false;
+        }
+        // 机场
+        if (this.airport && v.org_airport_name !== this.airport) {
+          vaild = false;
+        }
+        // 出发时间
+        if (this.flightTimes) {
+          const [from, to] = this.flightTimes.split(","); //['0','6']
+          const start = +v.dep_time.split(":")[0];
+          if (start < from || start > to) {
+            vaild = false;
+          }
+        }
+        // 机型
+        if (this.airSize && v.plane_size !== this.airSize) {
+          vaild = false;
+        }
+        return vaild;
+      });
+      this.$emit("getData", result);
+      return "";
+    }
+  },
   methods: {
     // 选择机场时候触发
     handleAirport(value) {
-      const arr = this.data.flights.filter(v => {
-        return v.org_airport_name === value;
-      });
-      //   console.log(arr);
-      this.$emit("getData", arr);
+      //   const arr = this.data.flights.filter(v => {
+      //     return v.org_airport_name === value;
+      //   });
+      //   //   console.log(arr);
+      //   this.$emit("getData", arr);
     },
 
     // 选择出发时间时候触发
     handleFlightTimes(value) {
       // console.log(value);
-      const [from, to] = value.split(","); //['0','6']
-      // console.log([start,end]);
-      const arr = this.data.flights.filter(v => {
-        //   +v.dep_time.split(":")[0]  前面加个‘+’让其变为number类型
-        const start = +v.dep_time.split(":")[0];
-        // console.log(start);
-        return start >= from && start < to;
-      });
-      this.$emit("getData", arr);
+      //   const [from, to] = value.split(","); //['0','6']
+      //   // console.log([start,end]);
+      //   const arr = this.data.flights.filter(v => {
+      //     //   +v.dep_time.split(":")[0]  前面加个‘+’让其变为number类型
+      //     const start = +v.dep_time.split(":")[0];
+      //     // console.log(start);
+      //     return start >= from && start < to;
+      //   });
+      //   this.$emit("getData", arr);
     },
 
     // 选择航空公司时候触发
     handleCompany(value) {
-      console.log(this.data);
-      const arr = this.data.flights.filter(v => {
-        return v.airline_name === value;
-      });
-      //   console.log(arr);
-      this.$emit("getData", arr);
+      //   console.log(this.data);
+      //   const arr = this.data.flights.filter(v => {
+      //     return v.airline_name === value;
+      //   });
+      //   //   console.log(arr);
+      //   this.$emit("getData", arr);
     },
 
     // 选择机型时候触发
     handleAirSize(value) {
-      const arr = this.data.flights.filter(v => v.plane_size === value);
-      this.$emit("getData", arr);
+      //   const arr = this.data.flights.filter(v => v.plane_size === value);
+      //   this.$emit("getData", arr);
     },
 
     // 撤销条件时候触发
